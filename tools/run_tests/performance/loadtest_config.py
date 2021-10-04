18c766b31b54067f1cb83615597fbe1092d5388d
Paulo Castello da Costa <pcastello@google.com>
2021-10-04 07:45:01 -0700

    Rename `prefix` to `index` in index generator.

--- tools/run_tests/performance/loadtest_config.py
+++ tools/run_tests/performance/loadtest_config.py
@@ -111,10 +111,10 @@ def gen_run_indices(runs_per_test: int) -> Iterable[str]:
     if runs_per_test < 2:
         yield ''
         return
-    prefix_length = len('{:d}'.format(runs_per_test - 1))
-    prefix_fmt = '{{:0{:d}d}}'.format(prefix_length)
+    index_length = len('{:d}'.format(runs_per_test - 1))
+    index_fmt = '{{:0{:d}d}}'.format(index_length)
     for i in range(runs_per_test):
-        yield prefix_fmt.format(i)
+        yield index_fmt.format(i)
 
 
 def gen_loadtest_configs(
@@ -158,7 +158,8 @@ def gen_loadtest_configs(
             metadata['name'] = name
             if 'labels' not in metadata:
                 metadata['labels'] = dict()
-            metadata['labels']['language'] = safe_name(language_config.language)
+            metadata['labels']['language'] = safe_name(
+                language_config.language)
             metadata['labels']['prefix'] = prefix
             if 'annotations' not in metadata:
                 metadata['annotations'] = dict()
